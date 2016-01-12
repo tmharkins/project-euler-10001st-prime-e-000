251b45c310e66e1c45c1671076883e2d23c3d9f2
tmharkins <tmharkins@gmail.com>
2016-01-12 00:26:44 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,12 @@
-# Implement your procedural solution here!
+require 'prime'
+def prime_number_for(nth_element)
+	counter = 0
+	prime_count = 1
+  until prime_count > nth_element
+		if Prime.prime?(counter)
+			prime_count += 1
+		end
+		counter += 1
+	end
+	return counter-1
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,18 @@
-# Implement your object-oriented solution here!
+require 'prime'
+class Prime
+  def initialize(nth_element)
+		@nth_element = nth_element
+		@prime_count = 1
+		@counter = 0
+	end
+
+  def number
+		until @prime_count > @nth_element
+			if Prime.prime?(@counter)
+				@prime_count += 1
+			end
+			@counter += 1
+		end
+		return @counter-1
+	end
+end
